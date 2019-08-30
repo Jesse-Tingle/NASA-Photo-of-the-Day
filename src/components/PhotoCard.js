88f05db6140c1d7114d43c87440494cd22d51f4c
Jesse-Tingle <jtingle0@gmail.com>
2019-08-29 23:04:33 -0400

    install styled-components, add font family Nasalization, add styling to ContainerDiv, ImgAndCaptionDiv, StyledImg, StyleTitle

--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import { StyleTitle, ContainerDiv, StyledImg } from './styles.js';
+
+// const StyleTitle = styled.h2`
+//     color: blue;
+// `;
 
 function PhotoCard(props) {
     
     return (
-        <div>
-            <h2> { props.title }</h2>
-            <h4> { props.date } </h4>
-            <img src={ props.url } alt="NASA"/>
-            <p>All images provided by NASA</p>
-            <p> { props.explanation } </p>
-        </div>
+        <ContainerDiv>
+
+            <div>
+                <StyledImg src={ props.url } alt="NASA"/>
+                <p>All images provided by NASA</p>
+            </div>
+
+            <div>
+                <StyleTitle> { props.title } </StyleTitle>
+                <h4> { props.date } </h4>
+                <p> { props.explanation } </p>
+            </div>
+
+        </ContainerDiv>
     )
 }
 
---
+++ src/components/styles.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+
+
+
+export const ContainerDiv = styled.div`
+    display: flex;
+`;
+
+export const TextContentDiv = styled.div`
+
+`;
+
+export const ImgAndCaptionDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export const StyledImg = styled.img`
+    height: 500px;
+`;
+
+export const StyleTitle = styled.h2`
+    font-family: Nasalization;   
+    color: blue;
+`;
